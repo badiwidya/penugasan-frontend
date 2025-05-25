bdae1cb6499d05537f6f8642171d6337e7ad2380
mixinit <adiwidya.berton13@gmail.com>
2025-05-25 23:29:52 +0700

    Fix salah endpoint sama tambah class transition

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,13 +3,13 @@
         <div class="bg-surface rounded-md p-8 shadow-overlay flex flex-col max-w-120">
             <h1 class="text-2xl font-semibold text-mauve mb-4 text-center">Penugasan PI 61</h1>
             <p class="text-text text-wrap text-justify mb-4">Sebuah dashboard sederhana yang dibuat untuk mengelola penugasan Pekan Ilkomerz 61.</p>
-            <button @click="login" class="bg-green hover:-translate-y-0.5 flex gap-3 justify-center items-center text-surface py-2 rounded-md font-bold cursor-pointer hover:glow"><i class="pi pi-google font-bold"></i>Masuk Dengan Google</button>
+            <button @click="login" class="bg-green hover:-translate-y-0.5 flex gap-3 justify-center items-center text-surface py-2 rounded-md font-bold cursor-pointer transition-all duration-300"><i class="pi pi-google font-bold"></i>Masuk Dengan Google</button>
         </div>
     </main>
 </template>
 
 <script setup>
     function login() {
-        window.location.href = 'http://localhost:3000/auth/login'
+        window.location.href = 'http://localhost:3000/auth/google'
     }
 </script>
