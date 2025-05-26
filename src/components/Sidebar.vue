e57c397bc21449a7e73b8b8bc2917cf2644f6030
mixinit <adiwidya.berton13@gmail.com>
2025-05-26 07:11:36 +0700

    Buat sidebar dan dashboard layout

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+    <aside class="bg-crust min-h-full w-82 flex flex-col p-8 top-0 left-0">
+        <h1 class="font-bold text-mauve mb-4 text-2xl">Dashboard</h1>
+        <nav class="w-full flex flex-col gap-1">
+            <RouterLink to="/dashboard" :class="{ 'bg-surface': $route.path === '/dashboard' }" class="pl-4 py-2 text-text font-semibold rounded-md hover:bg-surface">Beranda</RouterLink>
+            <RouterLink to="/assignments" :class="{ 'bg-surface': $route.path === '/assignments' }" class="pl-4 py-2 text-text font-semibold rounded-md hover:bg-surface">Tugas</RouterLink>
+        </nav>
+    </aside>
+</template>
---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import Sidebar from '@/components/Sidebar.vue'
+</script>
+
+<template>
+    <div class="bg-base min-h-screen flex">
+        <Sidebar />
+        <div class="w-full">
+            <slot />
+        </div>
+    </div>
+</template>
