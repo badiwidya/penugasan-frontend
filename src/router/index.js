bd424cb87067d7942bf14b63857f1fcb2c1436e8
mixinit <adiwidya.berton13@gmail.com>
2025-05-26 09:34:43 +0700

    Tambah route dan view Assignment

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,62 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/LoginView.vue'
-import DashboardView from '@/views/DashboardView.vue'
-import { useAuth } from '@/stores/auth.js'
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "@/views/LoginView.vue";
+import DashboardView from "@/views/DashboardView.vue";
+import { useAuth } from "@/stores/auth.js";
+import AssignmentsView from "@/views/AssignmentsView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      meta: {
-        requiresAuth: true,
-      },
-      redirect: '/dashboard'
-    },
-    {
-      path: '/login',
-      name: 'login',
-      meta: {
-        title: 'Login Page - PI 61',
-      },
-      component: LoginView,
-    },
-    {
-      path: '/dashboard',
-      name: 'Dashboard',
-      meta: {
-        title: 'Dashboard - PI 61',
-        requiresAuth: true,
-      },
-      component: DashboardView,
-    },
-  ],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            meta: {
+                requiresAuth: true,
+            },
+            redirect: "/dashboard",
+        },
+        {
+            path: "/login",
+            name: "login",
+            meta: {
+                title: "Login Page - PI 61",
+            },
+            component: LoginView,
+        },
+        {
+            path: "/dashboard",
+            name: "Dashboard",
+            meta: {
+                title: "Dashboard - PI 61",
+                requiresAuth: true,
+            },
+            component: DashboardView,
+        },
+        {
+            path: "/assignments",
+            name: "Assignment",
+            meta: {
+                title: "Tugas - PI 61",
+                requiresAuth: true,
+            },
+            component: AssignmentsView,
+        },
+    ],
+});
 
 router.beforeEach(async (to, from, next) => {
-  const isLoggedIn = useAuth().authenticated
+    const isLoggedIn = useAuth().authenticated;
 
-  const defaultTitle = 'Pekan Ilkomerz 61'
-  document.title = to.meta.title || defaultTitle
+    const defaultTitle = "Pekan Ilkomerz 61";
+    document.title = to.meta.title || defaultTitle;
 
-  if (to.meta.requiresAuth && !isLoggedIn) {
-    next('/login')
-  } else if (to.path === '/login' && isLoggedIn) {
-    next('/dashboard')
-  } else {
-    next()
-  }
-})
+    if (to.meta.requiresAuth && !isLoggedIn) {
+        next("/login");
+    } else if (to.path === "/login" && isLoggedIn) {
+        next("/dashboard");
+    } else {
+        next();
+    }
+});
 
-export default router
+export default router;
