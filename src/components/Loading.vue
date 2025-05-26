27be6b5470c7483056441a6a9db52b37ac35f271
mixinit <adiwidya.berton13@gmail.com>
2025-05-27 00:44:30 +0700

    Set loading height agar tidak keluar batas

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -6,7 +6,7 @@ const props = defineProps(['classTambahan'])
 </script>
 
 <template>
-    <div :class="['flex justify-center items-center h-full gap-2 text-text', classTambahan]">
+    <div :class="['flex justify-center items-center h-[70%] gap-2 text-text', classTambahan]">
         <i class="pi pi-spin pi-spinner"></i>
         Mengambil data...
     </div>
