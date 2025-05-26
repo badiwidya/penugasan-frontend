483641ecfcc7d9520ee787f14e0a2ba05d2e7587
mixinit <adiwidya.berton13@gmail.com>
2025-05-26 18:31:01 +0700

    Pasang plugin pinia persistence

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import { useAuth } from './stores/auth.js'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 
 const auth = useAuth()
