8bb4e945899e09dcde1bab0af6d8aa07504d76b2
mixinit <adiwidya.berton13@gmail.com>
2025-05-28 09:53:48 +0700

    Ubah fetching di course store

--- src/stores/course.js
+++ src/stores/course.js
@@ -21,39 +21,9 @@ export const useCoursesStore = defineStore('courses', {
 
         async forceFetch() {
             try {
-                const res = await api.get('/api/courses')
-                const mappedData = res.data.data.map(async (course) => {
+                const response = await api.get('/api/courses')
 
-                    const [teachersRes, studentsRes] = await Promise.all([
-                        api.get(`/api/courses/${course.id}/teachers`),
-                        api.get(`/api/courses/${course.id}/students`)
-                    ])
-
-                    const teachers = teachersRes.data.data.map(({ profile }) => ({
-                        profile: {
-                            name: { fullName: profile.name.fullName },
-                            photoUrl: profile.photoUrl
-                        }
-                    }))
-
-                    const students = studentsRes.data.data.map(({ profile }) => ({
-                        profile: {
-                            name: { fullName: profile.name.fullName },
-                            photoUrl: profile.photoUrl
-                        }
-                    }))
-
-                    return {
-                        id: course.id,
-                        name: course.name,
-                        enrollmentCode: course.enrollmentCode,
-                        alternateLink: course.alternateLink,
-                        teachers: teachers,
-                        students: students
-                }
-
-                })
-                this.courses = await Promise.all(mappedData)
+                this.courses = response.data.data
             } catch (error) {
                 console.log('Terjadi kesalahan saat fetch ulang data kelas:', error?.message)
                 throw error
