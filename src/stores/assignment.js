0a9147912e4670607e3342a1c565b7859c0a469e
mixinit <adiwidya.berton13@gmail.com>
2025-05-28 09:54:45 +0700

    Ubah fetching di assignment  store

--- src/stores/assignment.js
+++ src/stores/assignment.js
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia'
 export const useAssignmentsStore = defineStore('assignments', {
     state: () => {
         return {
-            pairIds: [],
+            pairIds: {},
             assignments: {}
         }
     },
@@ -18,47 +18,10 @@ export const useAssignmentsStore = defineStore('assignments', {
 
         async forceFetch() {
             try {
-                const [assignmentsRes, topicsRes] = await Promise.all([
-                    api.get('/api/assignments'),
-                    api.get('/api/topics')
-                ])
+                const response = await api.get('/api/assignments')
 
-                const assignments = {}
-
-                topicsRes.data.data.forEach(topic => {
-                    if (!(topic in assignments)) {
-                        assignments[topic.name] = []
-                    }
-                })
-
-                const mappedData = Promise.all(assignmentsRes.data.data.map(async (t) => {
-
-                    const assignmentsForPairIds = t.assignments.map((a) => {
-
-                        const topicName = topicsRes.data.data.find((topic) => topic.id === a.topicId).name
-
-                        assignments[topicName].push(a)
-                        
-                        return {
-                            type: a.topicId,
-                            name: a.title,
-                            id: a.id,
-                            topicId: a.topicId
-                        }
-                    })
-
-                    const pairIds = {
-                        courseId: t.courseId,
-                        assignments: assignmentsForPairIds
-                    }
-
-                    return { pairIds }
-                }))
-
-                this.pairIds = await mappedData
-
-                this.assignments = assignments
-
+                this.pairIds = response.data.data.pairIds
+                this.assignments = response.data.data.assignments
             } catch (error) {
                 console.error(error)
                 throw error
